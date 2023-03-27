f5015cb3f173c8de2e91bb88c1fa072a08a2131a
Peterliu1457 <98079694+Peterliu1457@users.noreply.github.com>
2023-03-27 22:02:28 +0800

    Update forms.py

--- FYP_E-Share_NotYet-main/FYP-UI-Website-main/PythonUI-2--master/login-register-app-UI/app/forms.py
+++ FYP_E-Share_NotYet-main/FYP-UI-Website-main/PythonUI-2--master/login-register-app-UI/app/forms.py
@@ -28,3 +28,8 @@ class LoginForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')            
